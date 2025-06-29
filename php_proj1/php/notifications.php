33b3aaa74b51569b203f6db395491e9fcd5f036b
rdalx42 <hopartean.radu@spectrum.ro>
2025-06-29 11:39:22 +0300

    added notifications

--- php_proj1/php/notifications.php
+++ php_proj1/php/notifications.php
@@ -1,1 +1,70 @@
+<?php
+    session_start();
+    include("../php/db.php");
+?>
 
+<!doctype html>
+<html>
+
+<head>
+    <title>Notifications</title>
+    <meta charset="UTF-8">
+    <link rel="icon" href="./img/devit_icon.png" type="image/png" />
+    <link rel="stylesheet" href="../frontend/css/style.css" />
+    <script src="../frontend/js/theme.js"></script>
+</head>
+
+<body class="discord-theme">
+    <div class="sidebar">
+        <a href="../frontend/homepage.php">
+            <img src="../frontend/img/home.png" alt="Home" />
+        </a>
+
+        <a href="../frontend/dashboard.php">
+            <img src="../frontend/img/profile.png" alt="Profile" />
+        </a>
+
+        <a href="../php/create_com.php">
+            <img src="../frontend/img/com.png" alt="Create Community" />
+        </a>
+
+        <a href="../php/create_post.php">
+            <img src="../frontend/img/post.png" alt="Create Post" />
+        </a>
+
+        <a href="../php/notifications.php">
+            <img src="../frontend/img/bellicon.png" alt="Notifications">
+        </a>
+
+        <a href="../php/index.php">
+            <img src="../frontend/img/exit.png" alt="Logout" />
+        </a>
+    </div>
+
+    <div class="main-content">
+        <?php
+            $username = $_SESSION['username'];
+
+            $sql = 'SELECT user, content, subject FROM notifications WHERE user = "' . $username . '"';
+            $notification_result = mysqli_query($com_conn, $sql);
+
+            if ($notification_result && mysqli_num_rows($notification_result) > 0) {
+                $notifications = mysqli_fetch_all($notification_result, MYSQLI_ASSOC);
+
+                echo "<h1>Notifications:</h1>";
+
+                foreach ($notifications as $notification) {
+                    echo "<div class='user-display'>
+                        <p class='username'>u/" . htmlspecialchars($notification["subject"], ENT_QUOTES, 'UTF-8') .
+                        ": " . htmlspecialchars($notification["content"], ENT_QUOTES, 'UTF-8') . "</p>
+                        
+                    </div>";
+                }
+            } else {
+                echo "<p>No notifications found.</p>";
+            }
+        ?>
+    </div>
+</body>
+
+</html>
